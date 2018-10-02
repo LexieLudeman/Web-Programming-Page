309fa2e1ffc22b9b485429e65f65d6337936f95d
Lexie Ludeman <aludeman@mymail.mines.edu>
2018-10-02 11:40:20 -0600

    Added comments for todo

---
+++ jquery/quiz.js
@@ -0,0 +1,38 @@
+/* TO DO:
+
+PART 2:
+
+When the mouse enters over one of the question boxes, you must use jQuery to accomplish the following.  Do not use the :hover pseudo-class provided by CSS.  From this part forward, jQuery should be used to accomplish the desired tasks.  (You may have different CSS classes created to toggle styling.  But as stated, we don't want to use :hover.)
+
+    The background color of the box will change
+    The text in the question box will say "Click to see the question" (or similar wording)
+    When the mouse leaves the box, the normal background color is restored and the text in the question box is cleared
+
+
+
+Part 3:
+When the user clicks on a box to select a question:
+    The appropriate question is displayed in the question box
+    If the "Check Answer" button is clicked when true/false are not selected, display an alert with an appropriate message ("Must select true/false")
+    If the user attempts to choose another question without answering the current question, an alert will display ("Must answer current question")
+    If a question is currently displayed but has not been answered, when hovering over a question box change the background color but do NOT display "Click to see the question" (don't overwrite the current question the user needs to answer)
+
+
+Part 4:
+Once the user has selected an answer and pressed "Check Answer":
+
+    Display whether the answer is correct or not
+    The box for the question should be removed from the list
+    The question topic is displayed in the Questions Attempted sidebar
+    Reset the true/false buttons to be unselected
+
+
+Part 5:
+After all questions, have been answered:
+
+    Display some type of figure telling the user the game has ended.
+    When the user double clicks the figure, some interesting visual effect should happen.
+
+
+
+*/
